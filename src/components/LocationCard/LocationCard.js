ab395bca105cd97540b39b5cfec1e7362a24f6d4
thomasmunguya <thomasmunguya@gmail.com>
2025-07-17 02:28:19 +0200

    Reapply "Merge pull request #2 from oilchangers/ft-store-location-search"
    This reverts commit 1deb7b9e0811414cbddd813ca1855e9ecd811716.

---
+++ src/components/LocationCard/LocationCard.js
@@ -0,0 +1,57 @@
+import React, { forwardRef } from 'react';
+import PhoneIcon from '../Icons/PhoneIcon';
+import LocationPinIcon from '../Icons/LocationPinIcon';
+import DirectionIcon from '../Icons/DirectionIcon';
+import ClockIcon from '../Icons/ClockIcon';
+import ButtonLink from '../ButtonLink/ButtonLink';
+
+const LocationCard = forwardRef((props, ref) => {
+    return (
+        <div ref={ref} {...props} className="flex flex-col gap-4">
+            <div>
+                <h1 style={{ fontSize: '1.5rem' }} className="uppercase text-left">Oil Changers - {props.serviceDescription}</h1>
+                <div className="text-sm text-left text-gray-500 font-text">Oil Changers</div>
+            </div>
+
+            <div className="flex gap-4 items-center justify-between">
+                <div className="flex flex-col gap-4">
+                    <div className="flex items-center gap-1">
+                        <LocationPinIcon />
+                        <div>{props.address}</div>
+                    </div>
+
+                    <div className="flex items-center gap-1.5">
+                        <PhoneIcon />
+                        <div>{props.phone ?? 'N/A'}</div>
+                    </div>
+
+                    <div className="flex items-center gap-1.5">
+                        <ClockIcon />
+                        <div className={`${props.availability === 'Available Now' ? 'text-green-600' : props.availability === 'Short Wait Time' ? 'text-orange-400' : 'text-red-600'} font-bold`}>{props.availability}</div>
+                    </div>
+                </div>
+
+                <div className="flex flex-col items-center gap-1">
+                    <DirectionIcon width="24px" height="24px" />
+                    <div>{Number(props.distance).toFixed(2)} mi</div>
+                </div>
+            </div>
+
+            <div className="flex items-center gap-4">
+                <ButtonLink href={props.website} target="_blank" rel="noopener noreferrer" className="w-full">
+                    View Website
+                </ButtonLink>
+                <ButtonLink href={`https://www.google.com/maps/dir/?api=1&destination=${props.address}`}
+                    target="_blank" rel="noopener noreferrer"
+                    className="w-full"
+                >
+                    Get Directions
+                </ButtonLink>
+            </div>
+        </div>
+    );
+});
+
+LocationCard.displayName = 'LocationCard';
+
+export default LocationCard;
