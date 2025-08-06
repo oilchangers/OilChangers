8c0a3f84c2ccb8cc9ab4cd15fd464ee864454707
thomasmunguya <thomasmunguya@gmail.com>
2025-08-06 10:37:34 +0200

    Move constants to environment file.

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,5 +1,6 @@
 export const API_BASE_URL = 'https://api-staging.cinch.io/api/v1';//process.env.REACT_APP_SERVER_URL;
 export const ADMIN_API_BASE_URL = 'https://api-staging.cinch.io/admin/v1/';//process.env.REACT_APP_SERVER_URL;
 export const ACCESS_TOKEN_NAME = 'login_access_token';
-export const STORE_API_BASE_URL = 'https://localhost:44389/api/';
-export const STORE_API_KEY = 'Aj9EWHOxdQUe5WTDazQh3LyafN4YNLNI6pbVGugqpSLXOE6rJyBML0Aj1Ek2i4sX';
+export const STORE_API_BASE_URL = process.env.REACT_APP_STORE_API_BASE_URL;
+export const STORE_API_KEY = process.env.REACT_APP_STORE_API_KEY;
+export const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
