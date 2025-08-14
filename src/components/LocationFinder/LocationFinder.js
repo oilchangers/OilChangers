fe26e344a508908ec8c8fd9bc094abc7be46ef33
thomasmunguya <thomasmunguya@gmail.com>
2025-08-14 23:10:52 +0200

    Add parameters to store fetch request in LocationFinder.

--- src/components/LocationFinder/LocationFinder.js
+++ src/components/LocationFinder/LocationFinder.js
@@ -56,7 +56,7 @@ const LocationFinder = () => {
 
         setIsFetchingStores(true);
 
-        let url = `${STORE_API_BASE_URL}api/stores?cityOrZipCode=${cityOrZipCode}${locationTypesQueryParamsString}`;
+        let url = `${STORE_API_BASE_URL}api/stores?cityOrZipCode=${cityOrZipCode}${locationTypesQueryParamsString}&includeWaitTime=true&includeDistanceFromUserLocation=true&includeCoordinates=true`;
 
         await axios.get(url, {
             headers: {
@@ -149,97 +149,97 @@ const LocationFinder = () => {
     return (
         <div className="flex flex-col w-[100vw] h-[100vh]">
             <div className="flex-[3] flex w-[100%] min-h-[75vh]">
-            <div className="flex flex-col shadow-black shadow-md w-[20%]">
-                <div className="flex flex-col">
-                    <div className="flex flex-col gap-2">
-                        <h1 className="text-center text-4xl uppercase font-bold">Find an Oil Changers</h1>
-                        <form className="flex flex-col gap-2 px-5" onSubmit={(e) => {
-                            e.preventDefault();
-                            handleSearch(cityOrZipCode);
-                            setSearch(true);
-                        }}>
-                            <SearchBox
-                                onChange={(e) => setCityOrZipCode(e.target.value)}
-                                value={cityOrZipCode}
-                                placeholder="Enter city or zip code"
-                            />
-                            <Button type="submit">
-                                Find locations
-                            </Button>
-                        </form>
+                <div className="flex flex-col shadow-black shadow-md w-[20%]">
+                    <div className="flex flex-col">
+                        <div className="flex flex-col gap-2">
+                            <h1 className="text-center text-4xl uppercase font-bold">Find an Oil Changers</h1>
+                            <form className="flex flex-col gap-2 px-5" onSubmit={(e) => {
+                                e.preventDefault();
+                                handleSearch(cityOrZipCode);
+                                setSearch(true);
+                            }}>
+                                <SearchBox
+                                    onChange={(e) => setCityOrZipCode(e.target.value)}
+                                    value={cityOrZipCode}
+                                    placeholder="Enter city or zip code"
+                                />
+                                <Button type="submit">
+                                    Find locations
+                                </Button>
+                            </form>
 
-                        {isCurrentLocationSelectionVisible &&
-                            <div className="flex items-center gap-2 px-5">
-                                <MyLocationIcon width="1rem" height="1rem" />
-                                <div
-                                    className="underline cursor-pointer"
-                                    onClick={() => {
-                                        getCurrentUserLocation();
-                                        setHasUserRequestedCurrentLocation(true);
-                                        setCityOrZipCode(null);
-                                    }}
-                                >
-                                    Use my current location
+                            {isCurrentLocationSelectionVisible &&
+                                <div className="flex items-center gap-2 px-5">
+                                    <MyLocationIcon width="1rem" height="1rem" />
+                                    <div
+                                        className="underline cursor-pointer"
+                                        onClick={() => {
+                                            getCurrentUserLocation();
+                                            setHasUserRequestedCurrentLocation(true);
+                                            setCityOrZipCode(null);
+                                        }}
+                                    >
+                                        Use my current location
+                                    </div>
+                                </div>}
+                        </div>
+
+                        {stores.length > 0 &&
+                            <div>
+                                <div className="border-t border-gray-400 mb-3"></div>
+
+                                <div className="text-left font-bold mb-2 px-5">Filter locations</div>
+
+                                <div className="flex flex-wrap items-center justify-between gap-5 px-5">
+                                    <div className="flex gap-2 items-center">
+                                        <input type="checkbox" name="oil-changers" className="w-4 h-4" onChange={(e) => handleGetStoresByLocationType(e, 'Oil Changers')} />
+                                        <label htmlFor="oil-changers">Oil Changers</label>
+                                    </div>
+
+                                    <div className="flex gap-2 items-center">
+                                        <input type="checkbox" name="oil-changers-plus-repair" className="w-4 h-4" onChange={(e) => handleGetStoresByLocationType(e, 'Oil Changers + Repair')} />
+                                        <label htmlFor="oil-changers-plus-repair">Oil Changers + Repair</label>
+                                    </div>
+
+                                    <div className="flex gap-2 items-center">
+                                        <input type="checkbox" name="car-wash" className="w-4 h-4" onChange={(e) => handleGetStoresByLocationType(e, 'Oil Changers & Car Wash')} />
+                                        <label htmlFor="car-wash">Car Wash</label>
+                                    </div>
+
+                                    <div className="flex items-center justify-start gap-2">
+                                        <input type="checkbox" name="coming-soon" className="w-4 h-4" onChange={(e) => handleGetStoresByLocationType(e, 'Coming Soon')} />
+                                        <label htmlFor="coming-soon">Coming Soon</label>
+                                    </div>
                                 </div>
                             </div>}
+
+                        <div className="border-t border-gray-400 mt-3"></div>
                     </div>
 
-                    {stores.length > 0 &&
-                        <div>
-                            <div className="border-t border-gray-400 mb-3"></div>
-
-                            <div className="text-left font-bold mb-2 px-5">Filter locations</div>
-
-                            <div className="flex flex-wrap items-center justify-between gap-5 px-5">
-                                <div className="flex gap-2 items-center">
-                                    <input type="checkbox" name="oil-changers" className="w-4 h-4" onChange={(e) => handleGetStoresByLocationType(e, 'Oil Changers')} />
-                                    <label htmlFor="oil-changers">Oil Changers</label>
+                    {isFetchingStores ?
+                        <div className="text-left px-3 mt-3">Loading...</div> :
+                        stores.length > 0 ?
+                            <div className="flex flex-col gap-2 overflow-y-scroll">
+                                {stores.map((store) => (
+                                    <div key={store.id}>
+                                        <div className="m-3 px-4">
+                                            <SummarizedLocationCard {...store} className="px-3" />
+                                        </div>
+                                        <div className="border-t border-gray-400 mt-3" />
+                                    </div>
+                                ))}
+                            </div> :
+                            !cityOrZipCode && !canAccessCurrentUserLocation ?
+                                <div className="text-left text-sm mt-3">
+                                    We cannot find your current location. Please search for a location above.
+                                </div> :
+                                <div className="text-left text-sm mt-3 text-gray-500 px-3">
+                                    No locations found within 50 miles of your location
                                 </div>
-
-                                <div className="flex gap-2 items-center">
-                                    <input type="checkbox" name="oil-changers-plus-repair" className="w-4 h-4" onChange={(e) => handleGetStoresByLocationType(e, 'Oil Changers + Repair')} />
-                                    <label htmlFor="oil-changers-plus-repair">Oil Changers + Repair</label>
-                                </div>
-
-                                <div className="flex gap-2 items-center">
-                                    <input type="checkbox" name="car-wash" className="w-4 h-4" onChange={(e) => handleGetStoresByLocationType(e, 'Oil Changers & Car Wash')} />
-                                    <label htmlFor="car-wash">Car Wash</label>
-                                </div>
-
-                                <div className="flex items-center justify-start gap-2">
-                                    <input type="checkbox" name="coming-soon" className="w-4 h-4" onChange={(e) => handleGetStoresByLocationType(e, 'Coming Soon')} />
-                                    <label htmlFor="coming-soon">Coming Soon</label>
-                                </div>
-                            </div>
-                        </div>}
-
-                    <div className="border-t border-gray-400 mt-3"></div>
+                    }
                 </div>
 
-                {isFetchingStores ?
-                    <div className="text-left px-3 mt-3">Loading...</div> :
-                    stores.length > 0 ?
-                        <div className="flex flex-col gap-2 overflow-y-scroll">
-                            {stores.map((store) => (
-                                <div key={store.id}>
-                                    <div className="m-3 px-4">
-                                            <SummarizedLocationCard {...store} className="px-3" />
-                                    </div>
-                                    <div className="border-t border-gray-400 mt-3" />
-                                </div>
-                            ))}
-                        </div> :
-                        !cityOrZipCode && !canAccessCurrentUserLocation ?
-                            <div className="text-left text-sm mt-3">
-                                We cannot find your current location. Please search for a location above.
-                            </div> :
-                            <div className="text-left text-sm mt-3 text-gray-500 px-3">
-                                No locations found within 50 miles of your location
-                            </div>
-                }
-            </div>
-
-            <Map selectedUserLocation={selectedUserLocation} stores={stores} isFetchingStores={isFetchingStores} />
+                <Map selectedUserLocation={selectedUserLocation} stores={stores} isFetchingStores={isFetchingStores} />
             </div>
 
             <LocationsByStateFooter className="flex-[1] border-solid border border-red-400" />
