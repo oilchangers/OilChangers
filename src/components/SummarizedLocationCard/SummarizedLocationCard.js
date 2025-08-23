52017a1fc4ddac93e80e3e91d17696d4a83cb6e8
thomasmunguya <thomasmunguya@gmail.com>
2025-08-23 11:40:08 +0200

    Cleanup SummarizedLocationCard component.

--- src/components/SummarizedLocationCard/SummarizedLocationCard.js
+++ src/components/SummarizedLocationCard/SummarizedLocationCard.js
@@ -4,46 +4,71 @@ import LocationPinIcon from '../Icons/LocationPinIcon';
 import DirectionIcon from '../Icons/DirectionIcon';
 import ButtonLink from '../ButtonLink/ButtonLink';
 import WaitTime from '../WaitTime/WaitTime';
+import { kebabCase } from 'change-case';
 
 const SummarizedLocationCard = forwardRef((props, ref) => {
 
     return (
-        <div ref={ref} className={`flex flex-col gap-4 ${props?.className}`}>
+        <div ref={ref} className={`flex flex-col gap-3 ${props?.className}`}>
             <div>
-                <h1 style={{ fontSize: '1.5rem' }} className="uppercase text-left">{props.locationName}</h1>
-                <div className="text-sm text-left text-gray-500 font-text">{props.locationType}</div>
+                <h1 className="uppercase text-left text-sm">{props.locationName}</h1>
+                <div className="text-[0.625rem] text-left text-gray-500 font-text">{props.locationType}</div>
             </div>
 
-            <div className="flex gap-4 items-center justify-between">
-                <div className="flex flex-col gap-4">
-                    <div className="flex items-center gap-1">
-                        <LocationPinIcon />
-                        <div>{props.addressLine1}</div>
+            <div className="flex justify-between">
+                <div className="flex flex-col gap-3">
+                    <div className="flex justify-between">
+                        <div className="flex items-center gap-1">
+                            <LocationPinIcon height="0.625rem" width="0.625rem" />
+                            <a href={`https://www.google.com/maps/dir/?api=1&destination=${props.addressLine1}`}
+                                target="_blank" rel="noopener noreferrer"
+                                className="hover:underline hover:text-primary"
+                            >
+                                {props.addressLine1}
+                            </a>
+                        </div>
                     </div>
 
                     <div className="flex items-center gap-1.5">
-                        <PhoneIcon />
-                        <div>{props.phoneNumber ?? 'N/A'}</div>
+                        <PhoneIcon height="0.625rem" width="0.625rem" />
+                        {props.phoneNumber ?
+                            <a href={`tel:${props.phoneNumber}`}
+                                className="hover:underline hover:text-primary"
+                            >
+                                {props.phoneNumber}
+                            </a> :
+                            <div>N/A</div>
+                        }
                     </div>
+
                     <WaitTime waitTime={props.waitTime} />
                 </div>
 
-                <div className="flex flex-col items-center gap-1">
-                    <DirectionIcon width="24px" height="24px" />
+                <a href={`https://www.google.com/maps/dir/?api=1&destination=${props.addressLine1}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex flex-col items-center hover:no-underline hover:text-black">
+                    <DirectionIcon width="1.25rem" height="1.25rem" />
                     <div>{props.distanceFromUserLocation ?
                         `${props.distanceFromUserLocation.value.toFixed(2)} ${props.distanceFromUserLocation.unit}` :
                         'N/A'}
                     </div>
-                </div>
+                </a>
             </div>
 
             <div className="flex items-center gap-4">
-                <ButtonLink href={props.websiteUrl} target="_blank" rel="noopener noreferrer" className="w-full">
+                <ButtonLink
+                    href={`/locations/${props.state.toLowerCase()}/${props.city.toLowerCase()}/${kebabCase(props.addressLine1)}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-full py-1">
                     View Website
                 </ButtonLink>
-                <ButtonLink href={`https://www.google.com/maps/dir/?api=1&destination=${props.addressLine1}`}
-                    target="_blank" rel="noopener noreferrer"
-                    className="w-full"
+                <ButtonLink
+                    href={`https://www.google.com/maps/dir/?api=1&destination=${props.addressLine1}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-full py-1"
                 >
                     Get Directions
                 </ButtonLink>
