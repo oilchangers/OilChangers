2162d5f06e77014da79bef064a87c6bd45d4cb67
thomasmunguya <thomasmunguya@gmail.com>
2025-08-26 12:03:41 +0200

    Make location browsing by city page responsive.

--- src/components/CitiesByState/CitiesByState.js
+++ src/components/CitiesByState/CitiesByState.js
@@ -2,10 +2,11 @@ import { useParams } from "react-router-dom";
 import { STORE_API_BASE_URL, STORE_API_KEY } from "../../constants/apiConstants";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const CitiesByState = () => {
     const { stateCode } = useParams();
-    const [state, setState] = useState([]);
+    const [state, setState] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
@@ -19,7 +20,6 @@ const CitiesByState = () => {
                     setState(response.data);
                 }
             }).catch((error) => {
-                console.log(error);
             }).finally(() => {
                 setIsLoading(false);
             })
@@ -29,19 +29,24 @@ const CitiesByState = () => {
     }, [stateCode]);
 
     return (
-        <div>
-            {
-                isLoading ? <div>Loading...</div> :
-                    <div>
-                        <h1 className="text-5xl uppercase">Oil Changers in {stateCode}</h1>
-                        <ul className="px-16 py-10 text-left leading-normal">
-                            {state.cities.map(city =>
-                                <li key={city} className="px-3 py-[0.4rem] font-bold text-sm">
-                                    <a href={`${stateCode}/${city}`}>{city}</a>
-                                </li>
-                            )}
-                        </ul>
+        <div className="w-screen h-screen">
+            {isLoading ?
+                <div className="flex justify-center items-center h-[90%]">
+                    <ClipLoader color="#fcca46" loading={isLoading} size={100} />
+                </div> :
+                <div className="flex flex-col max-w-[1250px] items-start gap-4 w-full pl-3 xl:ml-[27rem]">
+                    <div className="mb-8 mt-3 text-left">
+                        <a className="underline hover:text-primary font-bold" href={`/locations`}>Directory</a> <span>&gt;</span> {state.name}
                     </div>
+                    <h1 className="text-left text-[2.125rem] md:text-[3rem] lg:text-[2.75rem] leading-[1] uppercase">Oil Changers in {state.name}</h1>
+                    <ul className="px-[4px] md:pl-24 py-[1.5rem] text-left font-avenir-condensed font-semibold leading-normal">
+                        {state.cities.map(city =>
+                            <li key={city} className="px-3 py-[0.4rem]">
+                                <a className="hover:underline hover:text-primary" href={`${stateCode}/${city}`}>{city}</a>
+                            </li>
+                        )}
+                    </ul>
+                </div>
             }
         </div >
     )
