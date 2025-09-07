456b6b45aad770cfd7ecd445e09fe48581335fc2
thomasmunguya <thomasmunguya@gmail.com>
2025-09-07 21:39:48 +0200

    Connect store details page to SignalR hub.

--- src/pages/StoreDetails.js
+++ src/pages/StoreDetails.js
@@ -1,16 +1,18 @@
 import DetailedLocationCard from "../components/DetailedLocationCard/DetailedLocationCard";
 import GenericMap from "../components/Map/GenericMap";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { sentenceCase } from "change-case";
 import { STORE_API_BASE_URL, STORE_API_KEY } from "../constants/apiConstants";
 import { DEFAULT_MAP_CENTER } from "../utils/constants";
+import { HubConnectionBuilder } from "@microsoft/signalr";
 
 const LocationDetails = () => {
     const { idOrAddress } = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [store, setStore] = useState(null);
+    const storeRef = useRef(null);
 
     useEffect(() => {
         const getStore = async () => {
@@ -22,6 +24,7 @@ const LocationDetails = () => {
                 }
             }).then(response => {
                 if (response.status === 200) {
+                    storeRef.current = response.data;
                     setStore(response.data);
 
                     document.title = response.data?.homepageTitleTag;
@@ -35,6 +38,31 @@ const LocationDetails = () => {
 
         getStore();
     }, [idOrAddress]);
+
+    useEffect(() => {
+        try {
+            const signalRConnection = new HubConnectionBuilder()
+                .withUrl(`${STORE_API_BASE_URL}store-notification-hub`, {
+                    accessTokenFactory: () => STORE_API_KEY,
+                    withCredentials: true
+                })
+                .withAutomaticReconnect()
+                .build();
+
+            signalRConnection.on("StoreWaitTimeChanged", (message) => {
+                setStore(prev => ({
+                    ...prev,
+                    waitTime: message.waitTime
+                }));
+            });
+
+            signalRConnection.start();
+        }
+        catch (ignore) {
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     useEffect(() => {
         document.title = "Loading...";
